edc092cbba64fb469092d9f39b66d3da29701a20
danielleaharon <ah.danielle22@gmail.com>
2021-04-20 12:09:32 +0300

    2

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT ;
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send(JSON.stringify({ identity: 'Mockpress'}));
+    res.send(JSON.stringify({ identity: 'Mockpffffress'}));
 });
 
+console.log(process.env.PORT)
+
 app.listen(port, function () {
-    console.log('Mockpress is listning...');
+    console.log('Mockpress is fffffflistning...'+port);
 });
